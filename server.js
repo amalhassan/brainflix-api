5ff75f4d765984e861ce310306d8d4800e4bf365
Amal Hassan <amalmhass@gmail.com>
2023-03-25 08:10:54 -0400

    added variables and middleware to server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const PORT = 8080;
+const videosRoutes = require("./routes/videos.js")
+const cors = require("cors");
+app.use(express.static('public'));
+app.use(cors());
+
+app.use(express.json());
+app.use("/videos", videosRoutes);
+app.listen(PORT, () => {
+    console.log(`Listening at ports: ${PORT}`);
+});
