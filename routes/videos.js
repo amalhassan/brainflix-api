0e3db5eac099437670cea5d3d50cadbfeb9735e2
Amal Hassan <amalmhass@gmail.com>
2023-03-26 23:12:25 -0400

    added routes for comment section

--- routes/videos.js
+++ routes/videos.js
@@ -18,7 +18,7 @@ router.get("/", (_req, _res) => {
     const videoPlaylist = videos.map(({id, title, channel, image}) => ({id, title, channel, image}))
     _res.json(videoPlaylist);
 })
-//g et details of a specifiv video
+//get details of a specifiv video
 router.get("/:id", (req, res) => {
     const videos = videoDetails();
     const findVideo = videos.find((video) => video.id === req.params.id);
@@ -32,12 +32,12 @@ router.post("/", (req, res) => {
         id: uuid(),
         title: req.body.title,
         channel: "Mohan Murage",
-        image: "../Upload-video-preview.jpg",
+        image: "http://localhost:8080/Upload-video-preview.jpg",
         description: req.body.description,
         views:0,
         likes: 0,
         duration: "5:17",
-        video: "../BrainStation-Sample-Video.mp4",
+        video: "http://localhost:8080/BrainStation-Sample-Video.mp4",
         timestamp: Date.now(),
         comments: []
     }
@@ -45,6 +45,36 @@ router.post("/", (req, res) => {
     videos.push(newVideo);
     fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
     res.status(200).json(newVideo);
+})
+// get comments of specific video
+router.get("/:id/comments", (req, res) => {
+    const videos = videoDetails();
+    const findVideo = videos.find((video) => video.id === req.params.id);
+    // console.log(findVideo);
+    console.log(req.params.id);
+    res.json(findVideo.comments);
+})
+// post comment into json file
+router.post("/:id/comments", (req, res) => {
+    const newComment = {
+        id: uuid(),
+        name: "Mohan Murage",
+        comment: req.body.comment,
+        likes: 0,
+        timestamp: Date.now()
+    }
+    const videos = videoDetails();
+    const id = req.params.id;
+    console.log(id);
+    const updatedComments = videos.map(video => {
+        if (video.id === id) {
+            video.comments.push(newComment)
+        }
+        return video
+        } 
+    )
+    fs.writeFileSync("./data/videos.json", JSON.stringify(updatedComments));
+    res.status(200).json(updatedComments);
 })
 
 module.exports = router
